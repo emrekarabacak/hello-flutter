898492ac32567d9e3c7a5f8a4beebc40a20a9ee5
Emre Karabacak <emrekarabacak@Emres-MacBook-Pro.local>
2019-06-20 21:20:41 +0300

    basic widget added with pizza order scenario

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
-import './screens/home.dart';
+import './ui/home.dart';
 
-void main() => runApp(HelloFlutterApp());
-
-class HelloFlutterApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Hello Flutter App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Title is App Bar"),
-        ),
-        body: Home(),
-      ),
-    );
-  }
-}
+void main() => runApp(new MaterialApp(
+      title: "Simpe App",
+      home: Home(),
+    ));
